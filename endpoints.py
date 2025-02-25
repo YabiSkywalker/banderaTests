19b3d4620ba47a83a3078587440ad331e5e0f53d
Yabi <141942055+YabiSkywalker@users.noreply.github.com>
2025-02-25 12:04:40 -0600

    Update endpoints.py

--- endpoints.py
+++ endpoints.py
@@ -5,10 +5,34 @@ from pydantic import BaseModel, Field
 BASE_URL = "https://bandera-svc.yabi.dev"
 ##########################################
 
+##########################################
+AUTH_CONTROLLER = "auth"
+##########################################
+
+##########################################
+TICKET_CONTROLLER = "tickets"
+##########################################
+
+##########################################
+CUSTOMER_CONTROLLER = "customers"
+##########################################
+
+##########################################
+EMPLOYEES_CONTROLLER = "employees"
+##########################################
+
+##########################################
+BILLING_CONTROLLER = "billing"
+##########################################
+
+##########################################
+QUERY_CONTROLLER = "query"
+##########################################
+
 
 # Authentication Controller
-login_endpoint = "auth/login"
-register_endpoint = "auth/register"
+login_endpoint = "{AUTH_CONTROLLER}/login"
+register_endpoint = "{AUTH_CONTROLLER}/register"
 
 
 # Ticket Controller
@@ -17,8 +41,15 @@ set_status_endpoint = "tickets/{id}/setStatus"
 set_appointment_endpoint = "tickets/{id}/setAppointment"
 add_services_endpoint = "tickets/{id}/addServices"
 update_assignee_endpoint = "tickets/updateAssignee"
+find_ticket_by_id_endpoint = "/tickets"
+get_all_tickets_endpoint = "/tickets"
+delete_ticket_endpoint = "/tickets/{id}/delete"
+delete_service = "/tickets/{id}/removeSerivces"  # Ex: "/tickets/{id}/removeServices?index=1"
 
 # Customer Controller
+add_new_customer_endpoint = ""
+
+
 
 # Billing Controller
 
